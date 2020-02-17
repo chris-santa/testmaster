8af02d1aafeb5c64fbea209ac2a55a0806a3c461
Christopher Santana <christopher.santana@nav.no>
2020-02-17 10:59:07 +0100

    Test change boogaleux

--- distribute_workflows.sh
+++ distribute_workflows.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 for repository in $REPOSITORIES; do
-  echo $GITHUB_REPOSITORY
-  echo $REPOSITORY
-  if [[ $REPOSITORY == $GITHUB_REPOSITORY || "navikt/$REPOSITORY" == $GITHUB_REPOSITORY ]]; then
-    echo "Should not distribute files to same repository. Skipping $REPOSITORY"
+  if [[ $repository == $GITHUB_REPOSITORY || "navikt/$repository" == $GITHUB_REPOSITORY ]]; then
+    echo "Should not distribute files to same repository. Skipping $repository"
   else
     ./push_workflow_files.sh $repository
   fi
