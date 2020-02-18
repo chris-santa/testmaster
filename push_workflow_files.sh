7ee6d03fca6cf4cebd0977cf260746900c4249f0
Christopher Santana <christopher.santana@nav.no>
2020-02-18 10:09:06 +0100

    Testk

--- push_workflow_files.sh
+++ push_workflow_files.sh
@@ -30,9 +30,23 @@ for file in ./.github/workflows/__DISTRIBUTED_*; do
   NEW_FILE_SHA=$(git hash-object $file)
 
   if [[ $EXISTING_FILE_SHA != $NEW_FILE_SHA ]]; then
+    FILES_TO_UPDATE="$FILES_TO_UPDATE$TARGET_FILE_NAME, "
     TREE_NODES="$TREE_NODES$(createNode $file),"
   fi
 done
+
+
+## Print status and exit if dry run
+if [[ $DRY_RUN == "true" ]]; then
+  if [[ -z $FILES_TO_UPDATE ]]; then
+    echo "Dry run for repository $REPOSITORY: No workflow files would have been added or changed."
+    exit 0
+  else
+    FILES_LIST="[$(echo FILES_TO_UPDATE | sed 's/, $//')]"
+    echo "Dry run for repository $REPOSITORY: These files would have been added or changed $FILES_LIST"
+    exit 0
+  fi
+fi
 
 
 ## Exit if no changes are to be made
